da8542587477543c25e3d82b31bab2c18426c946
Roger Parrales <rstevenpr@gmail.com>
2025-02-06 21:35:23 -0600

    first commit

---
+++ src/sections/Features.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Element } from 'react-scroll'
+import { features } from '../constants' 
+
+const Features = () => {
+  return (
+    <section> 
+        <Element name='features'>
+            <div className="container">
+                <div className="relative flex">
+                    {features.map((feature) => (
+                        <div className="" key={feature.id}>{feature.title}</div>
+                    ))}
+                </div>
+            </div>
+        </Element>
+    </section>
+  )
+}
+
+export default Features
