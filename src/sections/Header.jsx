1ef380b61170c9c504b2989cd22bbc8d731e9993
Roger Parrales <rstevenpr@gmail.com>
2025-02-10 21:25:18 -0600

    changes on features and nav header

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { clsx } from 'clsx'
 import { Link as LinkScroll } from 'react-scroll'
@@ -12,9 +12,22 @@ const NavLink = ({title}) => (
     
 const Header = () =>  {
     const [isOpen, setIsOpen] = useState(false);
+    const [hasScroll, setHasScroll] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setHasScroll(window.scrollY > 35)
+        }
+
+        window.addEventListener('scroll', handleScroll)
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
 
     return (
-        <header className='fixed top-0 left-0 z-50 w-full py-10'>
+        <header className={clsx( 'fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4', hasScroll && 'py-2 bg-black-100 backdrop-blur-[8px]' )}  >
             <div className='container flex h-14 items-center max-lg:px-5'>
                 <a className='lg:hidden flex-1 cursor-pointer z-2'>
                     <img src="/images/xora.svg" width={115} height={55} alt="Logo" />
